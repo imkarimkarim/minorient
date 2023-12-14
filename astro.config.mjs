6cc5aedf0cb6246d16ed1e53f42d42ce6bb107e9
imkarimkarim <imkarimkarim@pm.me>
2023-12-14 23:41:22 +0330

    ci: 👷 add CI build system

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://imkarimkarim.github.io',
+  base: '/minorient',
 });
