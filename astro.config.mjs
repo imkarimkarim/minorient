7dd8f038d44f3d80fc774e88d7d3fbfd1e177fa9
imkarimkarim <imkarimkarim@pm.me>
2023-12-20 18:11:01 +0330

    feat: ✨ PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import AstroPWA from '@vite-pwa/astro'
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), AstroPWA({
+    
+  })],
   site: 'https://imkarimkarim.github.io',
   base: '/minorient',
 });
