95989f822ec29793b638b940f71eaea38013b54c
imkarimkarim <imkarimkarim@pm.me>
2023-12-21 13:23:02 +0330

    refactor: prettier

--- src/funcs/downloadImage.ts
+++ src/funcs/downloadImage.ts
@@ -1,27 +1,27 @@
 export function downloadImage(imgElement: HTMLImageElement, filename: string) {
-    // Create a canvas element
-    let canvas = document.createElement("canvas");
-    let ctx: any = canvas.getContext("2d");
-  
-    // Set the canvas dimensions to match the image
-    canvas.width = imgElement.width;
-    canvas.height = imgElement.height;
-  
-    // Draw the image onto the canvas
-    ctx.drawImage(imgElement, 0, 0);
-  
-    // Convert the canvas content to a Blob
-    canvas.toBlob(function(blob) {
-      // Create a download link
-      let link = document.createElement("a");
-      if(!blob) return;
-      link.href = URL.createObjectURL(blob);
-      link.download = filename;
-  
-      // Trigger the download
-      link.click();
-  
-      // Clean up the URL object
-      URL.revokeObjectURL(link.href);
-    });
-  }
+  // Create a canvas element
+  let canvas = document.createElement("canvas");
+  let ctx: any = canvas.getContext("2d");
+
+  // Set the canvas dimensions to match the image
+  canvas.width = imgElement.width;
+  canvas.height = imgElement.height;
+
+  // Draw the image onto the canvas
+  ctx.drawImage(imgElement, 0, 0);
+
+  // Convert the canvas content to a Blob
+  canvas.toBlob(function (blob) {
+    // Create a download link
+    let link = document.createElement("a");
+    if (!blob) return;
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+
+    // Trigger the download
+    link.click();
+
+    // Clean up the URL object
+    URL.revokeObjectURL(link.href);
+  });
+}
